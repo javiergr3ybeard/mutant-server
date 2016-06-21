772d1d6e5d91397fa923e620f5c6b842d7a7d4ae
Jordan Tompkins <jtompkins28@gmail.com>
2016-06-21 15:05:52 -0400

    add fiebase reference library

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var http = require('http');
+var firebase = require('firebase');
 
 var app = express();
 var server = http.createServer(app);
 
+firebase.initializeApp({
+  serviceAccount: "firebase-credentials.json",
+  databaseUTL: "https://my-cewl-project.firebaseio.com/"
+});
+
 server.listen(3030, function() {
-  console.log(listen on 'http://localhose:3030');
+  console.log('listen on http://localhose:3030');
 });
